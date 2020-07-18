211018d4c7e66eba228bc4b09bf46a0209d48a11
heyu <heyu@qq.com>
2020-07-18 20:34:21 +0800

    2020.7.18 20:34已提交

---
+++ atcrowdfunding05-common-util/src/main/java/com/atguigu/crowd/util/CrowdUtil.java
@@ -0,0 +1,11 @@
+package com.atguigu.crowd.util;
+
+public class CrowdUtil {
+
+    public static String md5(String source){
+        if (source==null || source.length()==0){
+            new RuntimeException("字符串不合法！请不要传入空字符串");
+        }
+        return null;
+    }
+}
