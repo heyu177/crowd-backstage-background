b27cdc7722d5ae8d59fef13d3e9e01ab31c38a40
heyu <508540105@qq.com>
2020-07-26 21:54:22 +0800

    2020.7.26 21:54已提交

--- atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/AdminHandler.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/AdminHandler.java
@@ -2,8 +2,8 @@ package com.atguigu.crowd.mvc.handler;
 
 import com.atguigu.crowd.entity.Admin;
 import com.atguigu.crowd.service.api.AdminService;
+import com.atguigu.crowd.util.ResultEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +18,10 @@ public class AdminHandler {
     private AdminService adminService;
 
     @RequestMapping("/do/login")
-    public String doLogin(@RequestParam("loginAcct") String loginAcct, @RequestParam("userPswd") String userPswd, HttpSession session){
+    public ResultEntity<Object> doLogin(@RequestParam("loginAcct") String loginAcct, @RequestParam("userPswd") String userPswd, HttpSession session){
         // 如果账号密码不正确，getAdminByLoginAcct就会抛出异常
         Admin admin = adminService.getAdminByLoginAcct(loginAcct,userPswd);
         session.setAttribute("loginAdmin",admin);
-        return "success";
+        return ResultEntity.successWithoutData();
     }
 }
